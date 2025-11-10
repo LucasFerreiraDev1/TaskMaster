6b07cfe8ac81e384976d416050e39bb6c366550b
LucasFerreiraDev1 <LuksDevs@outlook.com.br>
2025-11-10 01:58:03 -0300

    .

---
+++ src/js/backend/app/task.js
@@ -0,0 +1,19 @@
+/* Criar script de destroy session e button logoff
+
+Cria class de Task
+Constructor:
+    nameTask
+    responsible
+    nameProject
+    description
+    date
+    priority
+    status
+    createDate
+
+Funções do class Task: 
+	createTask()
+	getTasks()
+	updateTask() 
+	deleteTask()
+*/

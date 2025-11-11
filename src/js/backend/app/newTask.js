370c6fea0e5ca7c30ac15bff334e513e3f48fc44
LucasFerreiraDev1 <LuksDevs@outlook.com.br>
2025-11-11 15:26:40 -0300

    .

--- src/js/backend/app/newTask.js
+++ src/js/backend/app/newTask.js
@@ -1,3 +1,4 @@
+import { modalNotify } from "../../frontend/utils.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const taskName = document.querySelector('#taskName');
@@ -9,19 +10,44 @@ document.addEventListener('DOMContentLoaded', () => {
     const allStatus = document.getElementsByName('status');
 
 
-    document.querySelector('#createTask').addEventListener('click', event => {
+    document.querySelector('#createTask').addEventListener('click', event => 
+    {
         event.preventDefault();
 
-        const status = '';
-        for(let i = 0; i <= allStatus.length; i++) {
-            if (allStatus[i].checked) {
+        let status = '';
+        for(let i = 0; i < allStatus.length; i++) 
+        {
+            if (allStatus[i].checked) 
+            {
                 status = allStatus[i].value;
             }
         }
-        
+
+        const fields = 
+        [
+            { element: taskName, label: "Nome da Tarefa" },
+            { element: responsible, label: "Responsável" },
+            { element: projectName, label: "Nome do Projeto" },
+            { element: description, label: "Descrição" },
+            { element: dueDate, label: "Data de Entrega" },
+            { element: priority, label: "Prioridade" },
+            { element: status, label: "Status"}
+        ];
+
+        for(let field of fields) 
+        {
+            if(field.element.value.trim() === "")
+            {
+                modalNotify('Algo deu errado!', `Preencha o campo ${field.label}`, 'error');
+                field.element.style.borderColor = 'red';
+                return false;
+            }
+        }
+
     });
     
-    document.querySelector('#cancelTask').addEventListener('click', () => {
+    document.querySelector('#cancelTask').addEventListener('click', () =>
+    {
         location.href = './pending.html';
     });
 
